3b39dab2757594268ef735eba7009de5278907bd
Isa Pires <162845881+IsabeleVitoriaPires@users.noreply.github.com>
2025-08-25 18:12:19 +0000

    update

---
+++ designpatterns/src/main/java/br/univille/observer/Publisher.java
@@ -0,0 +1,30 @@
+package br.univille.observer;
+
+public class Publisher {
+
+
+    private String[] subscribers;
+
+    public void setSubscribers(String[] subscribers) {
+        this.subscribers = subscribers;
+    }
+
+    public String[] getSubscribers() {
+        return subscribers;
+    }
+
+    public void addSubscribers(String value){
+        if (value == null) return false;
+        if (subscribers == null) subscribers = new String[3]; 
+            
+        } 
+           
+        
+        
+    
+    
+    public static void main(String[] args) {
+        
+    }
+
+}
